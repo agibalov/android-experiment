2bf4b8fac046dc60a2a009276e03011483ae6ff3
loki2302 <loki2302@loki2302.me>
2013-11-26 20:02:18 -0800

    Cleaning up

--- gen/me/loki2302/R.java
+++ gen/me/loki2302/R.java
@@ -28,10 +28,6 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int email=0x7f040001;
-        public static final int markdown_html_template=0x7f040004;
-        public static final int password=0x7f040002;
-        public static final int sign_in=0x7f040003;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;

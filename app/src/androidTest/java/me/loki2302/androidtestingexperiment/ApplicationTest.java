0f00a208d9aaf717bd4c7f3c3a484b845bc312da
loki2302 <loki2302@loki2302.me>
2014-08-14 22:57:46 +0400

    Added CalculatorIntentService and dummy CalculatorIntentServiceTest

--- app/src/androidTest/java/me/loki2302/androidtestingexperiment/ApplicationTest.java
+++ app/src/androidTest/java/me/loki2302/androidtestingexperiment/ApplicationTest.java
@@ -1,17 +1,10 @@
 package me.loki2302.androidtestingexperiment;
 
 import android.app.Application;
-import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
-import android.test.TouchUtils;
-import android.test.UiThreadTest;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-
 }
